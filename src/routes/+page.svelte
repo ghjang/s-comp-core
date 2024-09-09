afd77c755bf26753bfa4f9e829e506ba0dda46ee
ghjang <jmxexpert@hotmail.com>
2024-09-09 21:58:47 +0900

    Marquee 테스트 페이지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>s-comp-core Tests</h1>
+
+<ol>
+	<li>
+		Marquee
+		<ul>
+			<li><a href="Marquee">Marquee</a></li>
+		</ul>
+	</li>
+</ol>
---
+++ src/routes/Marquee/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Marquee from '$lib/Marquee/Marquee.svelte';
+</script>
+
+<Marquee text="Hello Marquee!" color="black" />
